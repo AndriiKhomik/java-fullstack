be99a96f8507b17a10b9751584def47c206843ef
Andrii <andrii.khomik@gmail.com>
2024-08-28 19:05:57 +0300

    fix

--- entry_point.sh
+++ entry_point.sh
@@ -8,10 +8,6 @@ source variables.sh
 
 ./scripts/pgsql_restore.sh 2024-08-19.dump $POSTGRES_USER $POSTGRES_PASSWORD
 
-gradle test
-
-./scripts/pgsql_restore.sh 2024-08-19.dump $POSTGRES_USER $POSTGRES_PASSWORD
-
 cd /usr/local/tomcat/bin
 
 ./catalina.sh run
