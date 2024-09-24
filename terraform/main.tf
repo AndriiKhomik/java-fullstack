f5278fb642f971a452e039cc4ea26bb8b35659e4
Andrii <andrii.khomik@gmail.com>
2024-09-24 20:52:37 +0300

    combine with nginx

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,22 +16,26 @@ resource "docker_network" "network" {
 }
 
 
-# resource "docker_image" "nginx" {
-#   name = "nginx_image"
-#   build {
-#     path       = "../nginx/"
-#     dockerfile = "Dockerfile"
-#   }
-# }
+resource "docker_image" "nginx" {
+  name = "nginx_image"
+  build {
+    path       = "../nginx/"
+    dockerfile = "Dockerfile"
+  }
+}
 
-# resource "docker_container" "nginx" {
-#   name  = "nginx"
-#   image = docker_image.nginx.image_id
-#   ports {
-#     internal = 80
-#     external = 8080
-#   }
-# }
+resource "docker_container" "nginx" {
+  name  = "nginx"
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = 8080
+  }
+  networks_advanced {
+    name = docker_network.network.name
+  }
+  depends_on = [docker_container.frontend]
+}
 
 resource "docker_image" "frontend" {
   name = "frontend_image"
