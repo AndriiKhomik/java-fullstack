37fca56ac2f4d09b43754cb439f929f8687a74ab
Andrii <andrii.khomik@gmail.com>
2024-09-24 20:21:25 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,19 @@ module "nginx" {
   source = "./modules/nginx"
 
   providers = {
-    docker = docker.host
+    docker = docker
   }
+}
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.23.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
 }
