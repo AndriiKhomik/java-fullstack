47237b1d48df55b2dac1add7efe4ddf8df4ec11d
Andrii <andrii.khomik@gmail.com>
2024-09-24 17:03:16 +0300

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,24 @@
-module "network" {
-  source = "./modules/network"
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.23.0"
+    }
+  }
 }
 
-module "nginx" {
-  source = "./modules/nginx"
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  name  = "SoftServe"
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = 8080
+  }
 }
